8d72efddcfbb5af03097aa0e5b918948520dc508
okimoto <okimoto@one-net.jp>
2022-11-24 14:44:53 +0900

    commit

--- resources/views/kessai.blade.php
+++ resources/views/kessai.blade.php
@@ -8,7 +8,7 @@
     <title>ぐっとWifi</title>
     <link rel="stylesheet" href="{{ secure_asset('/assets/css/reset.css') }}">
     <link rel="stylesheet" href="{{ secure_asset('/assets/css/validationEngine.jquery.css') }}">
-    <link rel="stylesheet" href="{{ asset('/assets/css/contents.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('/assets/css/contents.css') }}">
     <link rel="stylesheet" href="{{ secure_asset('/assets/css/tablet.css') }}">
     <link rel="stylesheet" href="{{ secure_asset('/assets/css/smart.css') }}">
     <script src="{{ url('https://code.jquery.com/jquery-1.11.3.min.js')}}"></script>
